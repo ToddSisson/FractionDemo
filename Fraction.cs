f8c9ae97ed6702903435ffb343a45225d3f07eff
Todd Sisson <todd.a.sisson@gmail.com>
2015-12-09 11:46:08 -0600

    Added initial files

---
+++ Fraction.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionDemo
+{
+    class Fraction
+    {
+        
+
+
+        public int wholeNumber { get; set; }
+        public int numerator { get; set; }
+        private int denominator;
+        public int Denominator
+        {
+            get
+            {
+                return denominator;
+            }
+            set
+            {
+                if (value == 0)
+                {
+                    value = 1;
+                }
+                denominator = value;
+            }
+        }
+
+        public Fraction(int wholeNumber, int numerator, int denominator)
+        {
+            this.wholeNumber = wholeNumber;
+            this.numerator = numerator;
+            Denominator = denominator;
+            Reduce(numerator, denominator);
+        }
+
+        public Fraction(int numerator, int denominator)
+        {
+            wholeNumber = 0;
+            this.numerator = numerator;
+            Denominator = denominator;
+            //Reduce(numerator, Denominator);
+        }
+
+        public Fraction()
+        {
+            wholeNumber = 0;
+            numerator = 0;
+            denominator = 1;
+        }
+
+        public void Reduce(int numerator, int denominator)
+        {
+            int gcd = getGCD(numerator, denominator);
+            this.numerator /= gcd;
+            this.denominator /= gcd;
+        }
+
+
+        private static int getGCD(int a, int b)
+        {
+            //Drop negative signs
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            //Return the greatest common denominator between two integers
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            if (a == 0)
+                return b;
+            else
+                return a;
+        }
+
+
+
+        private static int getLCD(int a, int b)
+        {
+            //Return the Least Common Denominator between two integers
+            return (a * b) / getGCD(a, b);
+        }
+
+
+        public static Fraction operator +(Fraction one, Fraction two)
+        {
+            int newOneNumerator = (one.denominator * one.wholeNumber) + one.numerator;
+            int newTwoNumerator = (two.denominator * two.wholeNumber) + two.numerator;
+
+            int LCD = getLCD(one.denominator, two.denominator);
+
+            newOneNumerator *= LCD / one.denominator;
+            newTwoNumerator *= LCD / two.denominator;
+
+            int newNumerator = newOneNumerator + newTwoNumerator;
+            int newDenominator = LCD;
+
+            int newWholeNumber = newNumerator / newDenominator;
+            newNumerator %= newDenominator;
+
+            return new Fraction(newWholeNumber, newNumerator, newDenominator);
+        }
+
+        public static Fraction operator *(Fraction one, Fraction two)
+        {
+            int newOneNumerator = (one.denominator * one.wholeNumber) + one.numerator;
+            int newTwoNumerator = (two.denominator * two.wholeNumber) + two.numerator;
+
+            int newNumerator = newOneNumerator * newTwoNumerator;
+            int newDenominator = one.denominator * two.denominator;
+
+            int newWholeNumber = newNumerator / newDenominator;
+            newNumerator %= newDenominator;
+
+            return new Fraction(newWholeNumber, newNumerator, newDenominator);
+        }
+
+        public override string ToString()
+        {
+            if (wholeNumber == 0 && numerator == 0)
+                return ($"0");
+            else if (wholeNumber == 0)
+                return ($"{numerator} / {denominator}");
+            else if (numerator == 0)
+                return ($"{wholeNumber}");
+            else
+                return ($"{wholeNumber} {numerator} / {denominator}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction[] fractions = new Fraction[4];
+
+            for (int x = 0; x < fractions.Length; x++)
+            {
+                Console.WriteLine();
+                Console.Write("Whole Number: ");
+                int wholeNumber = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Numerator: ");
+                int numerator = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Denominator: ");
+                int denominator = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                fractions[x] = new Fraction(wholeNumber, numerator, denominator);
+                
+            }
+
+            for (int x = 0; x < fractions.Length; x++)
+            {
+                Fraction fraction1 = fractions[x];
+                for (int i = 0; i < fractions.Length; i++)
+                {
+                    Console.WriteLine();
+                    Fraction fraction2 = fractions[i];
+                    Fraction fraction3 = fraction1 + fraction2;
+                    Fraction fraction4 = fraction1 * fraction2;
+
+                    Console.WriteLine(fraction1.ToString() + " + " + fraction2.ToString());
+                    Console.WriteLine(fraction3.ToString());
+                    Console.WriteLine(fraction1.ToString() + " * " + fraction2.ToString());
+                    Console.WriteLine(fraction4.ToString());
+
+                }
+            }
+        }
+    }
+}
